22ea61d64ddd3a9a57577e98740019f2567d17f4
mrudenko <rudenko.clearpay@gmail.com>
2021-05-08 13:45:16 +0200

    only show transfer menu to the admin

--- client/src/modules/components/wallet/Wallets.js
+++ client/src/modules/components/wallet/Wallets.js
@@ -2,11 +2,13 @@ import React, {useState} from 'react'
 import {Card} from "react-bootstrap";
 import TransferSection from "../transfer/TransferSection";
 import WalletHeader from "./WalletHeader";
+import {useSelector} from "react-redux";
 
 
 const Wallets = (props) => {
     const {user} = props;
     const wallets = user.wallets;
+    const isAdmin = useSelector(state => state.admin.isAdmin);
     const [activeWallet, setActiveWallet] = useState(wallets ? wallets[0] : undefined);
 
     function walletChangeHandler(wallet) {
@@ -21,9 +23,9 @@ const Wallets = (props) => {
             <Card.Body key={"wallet_card_body_" + user.id}>
                 <Card.Title key={"wallet_card_body_title_" + user.id}>Current balance
                     is: {activeWallet.balance}</Card.Title>
-                <Card.Text key={"wallet_card_body_text_" + user.id}>
+                {isAdmin && <Card.Text key={"wallet_card_body_text_" + user.id}>
                     <TransferSection user={user} originWallet={activeWallet}/>
-                </Card.Text>
+                </Card.Text>}
             </Card.Body>
         </Card>);
 }
